6088f05e9000d6be34710e5874ffc8a13313ac03
Dan Constantinescu <dan_constantinescu@outlook.com>
2017-10-19 12:03:41 +0300

    root query and fetch static data

--- schema.js
+++ schema.js
@@ -6,3 +6,49 @@ const {
     GraphQLList,
     GraphQLNonNull
 } = require('graphql');
+const _ = require('lodash');
+
+//Hardcoded data
+const customers = [
+    {id: '1', name: 'Jonh', email: 'joh.e@fff.com', age: 35},
+    {id: '2', name: 'Dan', email: 'dan.e@fff.com', age: 25},
+    {id: '3', name: 'Satan', email: 'satan.e@fff.com', age: 335}
+];
+
+//Customer Type
+const CustomerType = new GraphQLObjectType({
+    name: 'Customer',
+    fields: () => ({
+        id: {type: GraphQLString},
+        name: {type: GraphQLString},
+        email: {type: GraphQLString},
+        age: {type: GraphQLInt}
+    })
+})
+
+
+//Root Query
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        customer: {
+            type: CustomerType,
+            args: {
+                id: {type: GraphQLString},
+            },
+            resolve(parentValue, args){
+                return _.find(customers, {id: args.id});
+            }
+        },
+        customers: {
+            type: new GraphQLList(CustomerType),
+            resolve(parentValue, args){
+                return customers;
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+})
--- server.js
+++ server.js
@@ -9,6 +9,6 @@ app.use('/graphql', expressGraphQL({
     graphiql: true
 }))
 
-app.listen(4001, () => {
+app.listen(4000, () => {
     console.log('server is running');
 })
